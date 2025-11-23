4ba875e9ff39dec7beebebfd7c9c5bed8fd87c92
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-11-23 07:08:58 +0300

    Refactor factorial function and main execution
    Refactored the factorial function and adjusted the main execution block.

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,14 +2,11 @@
 import sys
 
 def factorial(n):
-    if n < 0:
-        raise ValueError("Factorial is not defined for negative numbers")
-    result = 1
-    while n > 1:
-        result *= n
-        n -= 1
-    return result
+	result = 1
+	while n > 1:
+		result *= n
+		n -= 1
+	return result
 
-if __name__ == "__main__":
-    f = factorial(int(sys.argv[1]))
-    print(f)
+f = factorial(int(sys.argv[1]))
+print(f)
